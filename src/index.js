1c6815088b85ef273e11d06054d0dfe73f505611
phuoc <thaiphuoc1303@gmail.com>
2022-06-10 21:30:48 +0700

    first commit

---
+++ src/index.js
@@ -0,0 +1,24 @@
+const path = require('path')
+const express = require('express')
+const app = express()
+const handlebar = require("express-handlebars")
+const morgan = require('morgan')
+const port = 3000
+
+app.use(morgan('combined'))
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.engine('.hbs', handlebar.engine({
+  extname :'.hbs'
+}))
+app.set('view engine', '.hbs')
+app.set('views', path.join(__dirname, './views'))
+
+
+app.get('/', (req, res) => {
+    res.render('home')
+  })
+  
+  app.listen(port, () => {
+    console.log(`Example app listening on port http://localhost:${port}`)
+  })
